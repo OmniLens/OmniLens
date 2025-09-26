8abf847e82e8c440460f02b82910ab769c7e5fe1
Christopher Zeuch <chriszeuch.cz@gmail.com>
2025-09-26 23:02:42 +0200

    feat: added vercel analytics & speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { AuthProvider } from "@/components/auth-provider";
 import { Databuddy } from '@databuddy/sdk';
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "OmniLens",
@@ -23,6 +25,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         clientId={process.env.NEXT_PUBLIC_DATABUDDY_CLIENT_ID!}
         enableBatching={true}
       />
+      <Analytics />
+      <SpeedInsights />
       </body>
     </html>
   );
