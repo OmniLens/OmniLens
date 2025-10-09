7fcf8dd734772f4c0c1ff121a122b986010c10aa
Christopher Zeuch <chriszeuch.cz@gmail.com>
2025-10-09 12:32:51 +0200

    auth scope prep

--- lib/auth.ts
+++ lib/auth.ts
@@ -23,6 +23,7 @@ export const auth = betterAuth({
     github: { 
       clientId: process.env.GITHUB_CLIENT_ID as string, 
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      // scope: ["repo"],
     }, 
   },
   session: {
